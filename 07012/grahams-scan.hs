b38673a58451d0102858c23a5101846d9d93ee6c
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-10-13 14:45:25 -0400

    partial commit: grahams scan method for convex hull

--- 07012/grahams-scan.hs
+++ 07012/grahams-scan.hs
@@ -1,55 +1,46 @@
 module GrahamsScan where
     import Prelude hiding (Left, Right)
+    import Data.List (sortBy)
 
     -- Cartesian coordinates x y
     data Ctn = Ctn Double Double
                deriving (Show)
 
-    -- Polar coordinates angle magnitude
-    data Plr = Plr Double Double
-               deriving (Show)
-
-    -- How much of a turn in degrees
-    data Turn = Left Double
-              | Right Double
+    -- Turn direction
+    data Turn = Left
+              | Right
               | None
                 deriving (Show)
 
-    polar :: Ctn -> Plr
-    polar (Ctn x y) = Plr angle magnitude where
-        magnitude = sqrt (x * x + y * y)
-        d = degrees $ atan (y / x)
-        angle
-            | x >= 0          = 90 - d
-            | x < 0 && y >= 0 = 270 - d
-            | x < 0 && y < 0  = 270 - d
-
-    radians :: Double -> Double
-    radians = (*) (2 * pi / 360)
-
-    degrees :: Double -> Double
-    degrees = (*) (360 / 2 / pi)
-
-    deadreckon :: [Ctn] -> [Plr]
-    deadreckon xs = [polar $ delta a b | (a,b) <- zip as bs] where
-        as = take n $ drop 0 $ cycle xs
-        bs = take n $ drop 1 $ cycle xs
-        n = length xs
-
-    delta :: Ctn -> Ctn -> Ctn
-    delta (Ctn xa ya) (Ctn xb yb) = Ctn (xb - xa) (yb - ya)
-
-    pairs :: [Plr] -> [(Plr, Plr)]
-    pairs xs = zip as bs where
-        as = take n $ drop 0 $ cycle xs
-        bs = take n $ drop 1 $ cycle xs
-        n = length xs
-
-    -- test data: square, polygons need to be unclosed
-    polygon =
+    -- test data
+    points =
         [ Ctn 0 0
         , Ctn 1 0
         , Ctn 1 1
         , Ctn 0 1
+        , Ctn (-0.5) (-0.5)
+        , Ctn   0.5  (-0.5)
+        , Ctn   1.5  (-0.5)
+        , Ctn (-0.5)   0.5
+        , Ctn   0.5    0.5
+        , Ctn   1.5    0.5
+        , Ctn (-0.5)   1.5
+        , Ctn   0.5    1.5
+        , Ctn   1.5    1.5
         ]
 
+    first :: [Ctn] -> Ctn
+    first xs = head $ sortBy f xs where
+        f (Ctn xa ya) (Ctn xb yb)
+            | ya < yb             = LT
+            | ya == yb && xa < xb = LT
+            | ya > yb             = GT
+            | ya == yb && xa > xb = GT
+            | otherwise           = EQ
+
+    turn :: Ctn -> Ctn -> Ctn -> Turn
+    turn (Ctn xa ya) (Ctn xb yb) (Ctn xc yc)
+        | x == 0 = None
+        | x > 0  = Left
+        | x < 0  = Right
+        where x = (xb - xa) * (yc - ya) - (yb - ya) * (xc - xa)
