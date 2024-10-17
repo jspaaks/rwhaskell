d077726acc386fced9e9b971d75657024764b9fe
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-10-17 15:21:31 -0400

    solution to impossible torture exercise about graham scan

--- 07012/README.md
+++ 07012/README.md
@@ -2,38 +2,56 @@
 
 ## Interactive
 
-Below is some Haskell code that is useful for evaluating:
+Below are some Haskell snippets that are useful for evaluating:
+
+```haskell
+-- five pointed star for testing
+ctns = [Ctn (sin r * ell) (cos r * ell) | (ell, r) <- zip a b] where 
+    a = cycle [1, 0.6]
+    b = map (radians . (*) 36 ) [0,1..9]
+    radians = (*) (2 * pi / 360)
+```
 
 ```haskell
 -- print a list of cartesian coodinates
 printCtns :: [Ctn] -> IO ()
 printCtns ctns = putStr $ "[\n" <> f ctns <> "]\n" where
     f = unlines . map ((<>) ", ") . map show
+```
 
-
+```haskell
 -- print a list of triplets
 printTriplets :: [Triplet] -> IO ()
 printTriplets triplets = putStr $ "[\n" <> f triplets <> "]\n" where
     f = unlines . map ((<>) ", ") . map show
-
--- test data: two grids
-ctns1 =
-    [ Ctn 0 0
-    , Ctn 1 0
-    , Ctn 1 1
-    , Ctn 0 1
-    , Ctn (-0.5) (-0.5)
-    , Ctn   0.5  (-0.5)
-    , Ctn   1.5  (-0.5)
-    , Ctn (-0.5)   0.5
-    , Ctn   0.5    0.5
-    , Ctn   1.5    0.5
-    , Ctn (-0.5)   1.5
-    , Ctn   0.5    1.5
-    , Ctn   1.5    1.5
-    ]
 ```
 
+```haskell
+-- convert radians to degrees
+degrees = (*) (360 / 2 / pi)
+```
+
+```haskell
+-- convert degrees to radians
+radians = (*) (2 * pi / 360)
+```
+
+```haskell
+-- assign each cartesian point its own identifier
+[a,b,c,d,e,f,g,h,i,j,a'] = ctns
+```
+
+```haskell
+-- retrieve a tuple of x and y from a Ctn 
+unctn (Ctn x y) = (x,y)
+```
+
+```haskell
+-- print a list of Ctn in a format that gnuplot understands
+toGnuplot :: [Ctn] -> IO ()
+toGnuplot xs = putStr $ unlines $ map f xs where
+    f = \(Ctn x y) -> (show x) <> " " <> (show y)
+```
 
 ```console
 $ ghci
@@ -44,25 +62,78 @@ GHCi, version 9.4.7: https://www.haskell.org/ghc/  :? for help
 [1 of 1] Compiling GrahamScan       ( graham-scan.hs, interpreted )
 Ok, one module loaded.
 λ :{
-┣ ctns1 =
-┣     [ Ctn 0 0
-┣     , Ctn 1 0
-┣     , Ctn 1 1
-┣     , Ctn 0 1
-┣     , Ctn (-0.5) (-0.5)
-┣     , Ctn   0.5  (-0.5)
-┣     , Ctn   1.5  (-0.5)
-┣     , Ctn (-0.5)   0.5
-┣     , Ctn   0.5    0.5
-┣     , Ctn   1.5    0.5
-┣     , Ctn (-0.5)   1.5
-┣     , Ctn   0.5    1.5
-┣     , Ctn   1.5    1.5
-┣     ]
+┣ -- print a list of cartesian coodinates
+┣ printCtns :: [Ctn] -> IO ()
+┣ printCtns ctns = putStr $ "[\n" <> f ctns <> "]\n" where
+┣     f = unlines . map ((<>) ", ") . map show
 ┣ :}
-λ hull ctns1
-[Ctn (-0.5) (-0.5),Ctn 1.5 (-0.5),Ctn 1.5 1.5,Ctn (-0.5) 1.5,Ctn (-0.5) (-0.5)]
+λ :{
+┣ -- five pointed star for testing
+┣ ctns = [Ctn (sin r * ell) (cos r * ell) | (ell, r) <- zip a b] where 
+┣     a = cycle [1, 0.6]
+┣     b = map (radians . (*) 36 ) [0,1..9]
+┣     radians = (*) (2 * pi / 360)
+┣ :}
+λ printCtns ctns 
+[
+, Ctn 0.0 1.0
+, Ctn 0.3526711513754839 0.4854101966249684
+, Ctn 0.9510565162951535 0.30901699437494745
+, Ctn 0.5706339097770922 (-0.1854101966249684)
+, Ctn 0.5877852522924732 (-0.8090169943749473)
+, Ctn 7.347880794884119e-17 (-0.6)
+, Ctn (-0.587785252292473) (-0.8090169943749476)
+, Ctn (-0.570633909777092) (-0.18541019662496852)
+, Ctn (-0.9510565162951536) 0.30901699437494723
+, Ctn (-0.352671151375484) 0.48541019662496837
+]
+λ :{
+┣ toGnuplot :: [Ctn] -> IO ()
+┣ toGnuplot xs = putStr $ unlines $ map f xs where
+┣     f = \(Ctn x y) -> (show x) <> " " <> (show y)
+┣ :}
+λ toGnuplot ctns
+0.0 1.0
+0.3526711513754839 0.4854101966249684
+0.9510565162951535 0.30901699437494745
+0.5706339097770922 -0.1854101966249684
+0.5877852522924732 -0.8090169943749473
+7.347880794884119e-17 -0.6
+-0.587785252292473 -0.8090169943749476
+-0.570633909777092 -0.18541019662496852
+-0.9510565162951536 0.30901699437494723
+-0.352671151375484 0.48541019662496837
+λ printCtns $ hull $ ctns
+[
+, Ctn (-0.587785252292473) (-0.8090169943749476)
+, Ctn 0.5877852522924732 (-0.8090169943749473)
+, Ctn 0.9510565162951535 0.30901699437494745
+, Ctn 0.0 1.0
+, Ctn (-0.9510565162951536) 0.30901699437494723
+, Ctn (-0.587785252292473) (-0.8090169943749476)
+]
+λ toGnuplot $ hull $ ctns
+-0.587785252292473 -0.8090169943749476
+0.5877852522924732 -0.8090169943749473
+0.9510565162951535 0.30901699437494745
+0.0 1.0
+-0.9510565162951536 0.30901699437494723
+-0.587785252292473 -0.8090169943749476
 ```
+
+Some gnuplot code to visualize
+
+```
+$ gnuplot
+gnuplot> set xrange [-2:2]
+gnuplot> set yrange [-2:2]
+gnuplot> set size ratio -1
+gnuplot> plot 'all.txt' using 1:2:(0.1) with circles, 'hull.txt' using 1:2:(0.07) with circles fill solid noborder
+```
+
+Should result in:
+
+![convex hull for a five pointed star](hull.png)
 
 ## Linting
 
--- 07012/graham-scan.hs
+++ 07012/graham-scan.hs
@@ -37,7 +37,7 @@ module GrahamScan where
         bs = drop 1 $ xs
         cs = drop 2 $ xs
 
-
+    -- calculate the convex hull of a set of point using to graham's scan method
     hull xs = wrap $ f $ sorted $ nub xs where
 
         f :: [Ctn] -> [Ctn]
@@ -52,64 +52,7 @@ module GrahamScan where
         wrap xs = [ctn0] <> xs <> [ctn0]
         ctn0 = pivot $ nub $ xs
 
-
-    -- given an unsorted list of cartesian coordinates, sorts the list by increasing
-    -- angle relative to the reference point ctn0, discarding points that are
-    -- shadowed by other points on the same ray that are further away from the
-    -- reference point ctn0. Note the reference point itself is also discarded.
-    sorted :: [Ctn] -> [Ctn]
-    sorted ctns = removeProps $ furthest $ appendProps ctn0 ctns where
-
-        -- append properties needed for scan algorithm to each point
-        appendProps :: Ctn -> [Ctn] -> [Triplet]
-        appendProps ctn0 = triplets where
-            triplets = map g . filter f where
-                f = \ctn -> ctn /= ctn0
-                g = \ctn -> Triplet ctn (calcangle ctn ctn0) (calcdistsq ctn ctn0) where
-
-                    calcangle :: Ctn -> Ctn -> Angle
-                    calcangle (Ctn x y) (Ctn x0 y0) = degrees $ atan $ (y - y0) / (x - x0)
-
-                    calcdistsq :: Ctn -> Ctn -> Distance
-                    calcdistsq (Ctn x y) (Ctn x0 y0) = (x - x0)^2 + (y - y0)^2
-
-                    degrees :: Double -> Angle
-                    degrees = (*) (360 / 2 / pi)
-
-        -- deternmine the leftmost of the points of minimal y
-        ctn0 = pivot ctns
-
-        -- retain only the furthest point on each ray
-        furthest :: [Triplet] -> [Triplet]
-        furthest = map last . map sortByDistance . groupByAngle . sortByAngle where
-
-            -- sort the triplets by distance
-            sortByDistance :: [Triplet] -> [Triplet]
-            sortByDistance = sortBy f where
-                f = \a b -> compare (distance a) (distance b) where
-
-            -- group a list of triplets by angle
-            groupByAngle :: [Triplet] -> [[Triplet]]
-            groupByAngle = groupBy f where
-                f = \a b -> angle a == angle b
-
-            -- sort a list of triplets by angle
-            sortByAngle :: [Triplet] -> [Triplet]
-            sortByAngle = sortBy f where
-                f = \a b -> compare (angle a) (angle b)
-
-            -- getter for angle from Triplet
-            angle :: Triplet -> Angle
-            angle (Triplet _ a _) = a
-
-            -- getter for distance from Triplet
-            distance :: Triplet -> Distance
-            distance (Triplet _ _ d) = d
-
-        -- remove properties that were needed for scan algorithm from each point
-        removeProps = map (\(Triplet ctn _ _) -> ctn)
-
-
+    -- determine the leftmost of the points whose y value is minimal
     pivot :: [Ctn] -> Ctn
     pivot = head . sortBy f where
 
@@ -120,4 +63,65 @@ module GrahamScan where
             | ya > yb             = GT
             | ya == yb && xa > xb = GT
             | otherwise           = EQ
+
+
+    -- given an unsorted list of cartesian coordinates, sorts the list by increasing
+    -- angle relative to the reference point ctn0, discarding points that are
+    -- shadowed by other points on the same ray that are further away from the
+    -- reference point ctn0. Note the reference point itself is also discarded.
+    sorted :: [Ctn] -> [Ctn]
+    sorted ctns = removeProps $ furthest $ appendProps ctn0 ctns where
+
+        -- deternmine the leftmost of the points of minimal y
+        ctn0 = pivot ctns
+
+        -- append properties needed for scan algorithm to each point
+        appendProps :: Ctn -> [Ctn] -> [Triplet]
+        appendProps ctn0 = triplets where
+            triplets = map g . filter f where
+                f = \ctn -> ctn /= ctn0
+                g = \ctn -> Triplet ctn (calcangle ctn ctn0) (calcdistsq ctn ctn0) where
+
+                    calcangle :: Ctn -> Ctn -> Angle
+                    calcangle (Ctn x y) (Ctn x0 y0)
+                        | x < x0    = angle + 180
+                        | otherwise = angle
+                        where
+                            angle = degrees $ atan $ (y - y0) / (x - x0)
+
+                    calcdistsq :: Ctn -> Ctn -> Distance
+                    calcdistsq (Ctn x y) (Ctn x0 y0) = (x - x0)^2 + (y - y0)^2
+
+                    degrees :: Double -> Angle
+                    degrees = (*) (360 / 2 / pi)
+
+        -- retain only the furthest point on each ray
+        furthest :: [Triplet] -> [Triplet]
+        furthest = map last . map sortByDistance . groupByAngle . sortByAngle where
+
+            -- sort a list of triplets by angle
+            sortByAngle :: [Triplet] -> [Triplet]
+            sortByAngle = sortBy f where
+                f = \a b -> compare (angle a) (angle b)
+
+            -- group a list of triplets by angle
+            groupByAngle :: [Triplet] -> [[Triplet]]
+            groupByAngle = groupBy f where
+                f = \a b -> angle a == angle b where
+
+            -- getter for angle from Triplet
+            angle :: Triplet -> Angle
+            angle (Triplet _ a _) = a
+
+            -- sort the triplets by distance
+            sortByDistance :: [Triplet] -> [Triplet]
+            sortByDistance = sortBy f where
+                f = \a b -> compare (distance a) (distance b) where
+
+                    -- getter for distance from Triplet
+                    distance :: Triplet -> Distance
+                    distance (Triplet _ _ d) = d
+
+        -- remove properties that were needed for scan algorithm from each point
+        removeProps = map (\(Triplet ctn _ _) -> ctn)
 
