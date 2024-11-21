e0c187388ddcae302a4f2f726ed646e0e906219e
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-11-21 14:45:55 -0500

    added notes about io

---
+++ 16500/README.md
@@ -0,0 +1,16 @@
+# `16500`
+
+## Interactive
+
+```console
+$ runghc main.hs
+Greetings! What is your name?
+Azi
+Welcome to Haskell, Azi!
+```
+
+## Linting
+
+```console
+hlint main.hs
+```
---
+++ 16500/main.hs
@@ -0,0 +1,8 @@
+module Main (main) where
+
+    main :: IO ()
+    main = do
+        putStrLn "Greetings! What is your name?"
+        name <- getLine
+        putStrLn $ "Welcome to Haskell, " ++ name ++ "!"
+
